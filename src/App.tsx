b85a814c66d152509e0e731867a3a455dca5325c
Mihail Kotevski <mihail16kotevski@gmail.com>
2024-06-01 13:01:41 +0200

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
       description:
         "This project is a website where users can review different clothes from differentbrands, also users can read about the latest fashions in the market.",
       tools: ["Next.JS", "CSS", "Bootstrap"],
-      image: "./public/images/projects/Store.png",
+      image: "/images/projects/Store.png",
       github: "https://github.com/Mihail-Kotevski/MyProjects/tree/Store",
     },
     {
@@ -23,7 +23,7 @@ const App = () => {
       description:
         "This is a platform where users can explore a variety of dining options, receive suggestions, leave feedback about the chosen restaurant, and save their choice to favorites.This user-friendly interface makes it easy to discover new restaurants and leave feedback.",
       tools: ["React.JS", "SCSS"],
-      image: "./public/images/projects/Restaurant.png",
+      image: "public/images/projects/Restaurant.png",
       github: "https://github.com/Mihail-Kotevski/MyProjects/tree/Restaurant",
     },
     {
@@ -33,7 +33,7 @@ const App = () => {
       description:
         "This page was designed and coded by me. Here, you'll find a collection of some of my projects, a summary, and my contact information. This site serves as a portfolio to showcase my work and to provide an easy way for you to get in touch with me. ",
       tools: ["React.JS", "SCSS"],
-      image: "./public/images/projects/KotevskiDev.png",
+      image: "public/images/projects/KotevskiDev.png",
       github: "https://github.com/Mihail-Kotevski/MyProjects/tree/Portfolio",
     },
     {
@@ -53,7 +53,7 @@ const App = () => {
       description:
         "This project has two pages: Index page, and hire students page, and is responsive on tablet and mobile resolution. The index page has navbar with link that will lead you outside the page, and button that will lead you to the hire-student page. The index page also has a banner section, card filters and card section filled with cards that navigate to chosen Brainster Academy.",
       tools: ["JavaScript", "CSS", "Bootstrap"],
-      image: "dist/images/projects/Brainster Labs.png",
+      image: "public/images/projects/Brainster Labs.png",
       github:
         "https://github.com/Mihail-Kotevski/MyProjects/tree/BrainsterLabs",
     },
@@ -64,7 +64,7 @@ const App = () => {
       description:
         "This project was made to create web-space for the street artists to help them keep track of their income, and have a space on the web where they can showcase their masterpieces.",
       tools: ["JavaScript", "CSS", "Bootstrap"],
-      image: "/dist/images/projects/Street Artist.png",
+      image: "public/images/projects/Street Artist.png",
       github: "https://github.com/Mihail-Kotevski/MyProjects/tree/StreetArtist",
     },
   ];
