e8b59431bf32e959e45d6de947d2edd30748d7fc
Mihail Kotevski <mihail16kotevski@gmail.com>
2025-01-08 02:29:55 +0100

    Added routing and form files

--- brainster-labs-react/src/App.tsx
+++ brainster-labs-react/src/App.tsx
@@ -3,12 +3,17 @@ import "./App.css";
 import HomePage from "./pages/HomePage.tsx";
 import Header from "./components/header/Header.tsx";
 import Footer from "./components/footer/Footer.tsx";
+import { Route, Routes } from "react-router-dom";
+import Form from "./components/form/Form.tsx";
 
 function App() {
   return (
     <>
       <Header />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="hireStudent" element={<Form />} />
+      </Routes>
       <Footer />
     </>
   );
--- brainster-labs-react/src/components/header/Header.tsx
+++ brainster-labs-react/src/components/header/Header.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import "../header/header.css";
 import logo from "./../../images/header/Logo.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="nav-bar">
       <div className="content-wrapper">
         <div className="content">
-          <div className="logo">
+          <Link className="logo" to="/">
             <img src={logo} alt="" />
             <span>BRAINSTER</span>
-          </div>
+          </Link>
           <ul>
             <li>Academy for design</li>
             <li>Academy for coding</li>
             <li>Academy for markeeting</li>
             <li>Academy for data science</li>
           </ul>
-          <button>Hire a student</button>
+          <Link to="/hireStudent" className="btn">
+            Hire a student
+          </Link>
         </div>
       </div>
     </div>
