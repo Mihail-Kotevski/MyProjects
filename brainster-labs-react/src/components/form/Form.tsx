98319d661a17eda15ac7fde786bd8ee07b74b2f4
Mihail Kotevski <mihail16kotevski@gmail.com>
2025-01-08 03:00:17 +0100

    Added code for form

--- brainster-labs-react/src/components/form/Form.tsx
+++ brainster-labs-react/src/components/form/Form.tsx
@@ -2,5 +2,27 @@ import React from "react";
 import "../form/form.css";
 
 export default function Form() {
-  return <div className="form"></div>;
+  return (
+    <div className="form">
+      <h1>Hire Students</h1>
+      <form>
+        <div className="left">
+          <label>Full name</label>
+          <input type="text" />
+          <label>Email</label>
+          <input type="text" />
+          <label>Student Type</label>
+          <input type="text" />
+        </div>
+        <div className="right">
+          <label>Company name</label>
+          <input type="text" />
+          <label>Phone number</label>
+          <input type="text" />
+          <label className="button-label">/</label>
+          <button>Send</button>
+        </div>
+      </form>
+    </div>
+  );
 }
