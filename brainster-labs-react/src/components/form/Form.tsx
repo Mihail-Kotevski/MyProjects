75ac7fb41c632619af0d85c6e07f2883ce83c52f
Mihail Kotevski <mihail16kotevski@gmail.com>
2025-01-10 02:18:30 +0100

    added proper colors

--- brainster-labs-react/src/components/form/Form.tsx
+++ brainster-labs-react/src/components/form/Form.tsx
@@ -8,17 +8,17 @@ export default function Form() {
       <form>
         <div className="left">
           <label>Full name</label>
-          <input type="text" />
+          <input type="text" placeholder="Enter your full name!" />
           <label>Email</label>
-          <input type="text" />
+          <input type="text" placeholder="Enter your email adress!" />
           <label>Student Type</label>
-          <input type="text" />
+          <input type="text" placeholder="Enter student type!" />
         </div>
         <div className="right">
           <label>Company name</label>
-          <input type="text" />
+          <input type="text" placeholder="Enter company name!" />
           <label>Phone number</label>
-          <input type="text" />
+          <input type="text" placeholder="Enter your phone number!" />
           <label className="button-label">/</label>
           <button>Send</button>
         </div>
