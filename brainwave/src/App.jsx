664ac1fbbec12fcc6f9a30bcf97f6ef18a8e3bf6
Mihail Kotevski <mihail16kotevski@gmail.com>
2025-01-24 22:26:55 +0100

    Added reusable button component

--- brainwave/src/App.jsx
+++ brainwave/src/App.jsx
@@ -1,6 +1,17 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <ButtonGradient />
+      <div className="pt-[4,75rem] lg:p-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          something
+        </Button>
+      </div>
+    </>
+  );
+};
 
 export default App;
