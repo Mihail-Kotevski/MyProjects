d9e28de998d7c2c63db47ace0921bc90f1888022
Mihail Kotevski <mihail16kotevski@gmail.com>
2025-01-25 18:23:27 +0100

    Added header component

--- brainwave/src/App.jsx
+++ brainwave/src/App.jsx
@@ -1,15 +1,14 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <ButtonGradient />
       <div className="pt-[4,75rem] lg:p-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          something
-        </Button>
+        <Header />
       </div>
+      <ButtonGradient />
     </>
   );
 };
