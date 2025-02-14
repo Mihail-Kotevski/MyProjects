61f55df1cf37b26b4272912958a1c4922902c319
Mihail Kotevski <mihail16kotevski@gmail.com>
2025-02-14 19:04:30 +0100

    Fixed model view

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -56,7 +56,7 @@ const Model = () => {
         <h1 id="heading" className="section-heading">
           Take a closer look.
         </h1>
-        <div className="flex flex-col items-center mt-5">
+        <div className="flex flex-col items-center mt-5 overflow-hidden">
           <div className="w-full h-[75vh] md:h-[90vh} overflow-hidden relative">
             <ModelView
               index={1}
